138194a342f1ecc96203770e32ab47039219be86
Ravi Chauhan <ravi87104@gmail.com>
2025-07-03 15:45:32 +0530

    db chnge

--- db.js
+++ db.js
@@ -9,11 +9,10 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASS, // ✅ must match your ENV
   database: process.env.DB_NAME,
   port: process.env.DB_PORT
 });
-
 
 db.connect((err) => {
   if (err) throw err;
